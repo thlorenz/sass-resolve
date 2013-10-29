9324d0746111633ad819576a8bb7512b25e986d1
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-29 17:53:53 -0400

    two level index file imports in spike with source maps working

--- spikes/run.js
+++ spikes/run.js
@@ -8,15 +8,17 @@ var fixtures =  path.join(__dirname , 'fixtures');
 var foodir   =  path.join(fixtures, 'foo');
 var bardir   =  path.join(fixtures, 'bar');
 
-var foo_body_scss = foodir + '/body.scss';
-var foo_body_css = foodir + '/body.css';
+var foo_body_scss = foodir + '/index.scss';
 
-scss(foo_body_scss, foo_body_css, check);
+var index_scss = fixtures + '/index.scss';
+var index_css = fixtures + '/index.css';
+
+scss(index_scss, index_css, check);
 
 function check (err) {
   if (err) return console.error(err);
-  var foo_bodyScss = fs.readFileSync(foo_body_scss, 'utf8');
-  var foo_bodyCss = fs.readFileSync(foo_body_css, 'utf8');
+  var indexScss = fs.readFileSync(index_scss, 'utf8');
+  var indexCss = fs.readFileSync(index_css, 'utf8');
 
-  console.log('Rendered\n%s\n====\nTo:\n%s', foo_bodyScss, foo_bodyCss);
+  console.log('Rendered\n%s\n====\nTo:\n%s', indexScss, indexCss);
 }
