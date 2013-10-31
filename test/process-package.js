4a93e16c156e901e608d5307131cb42a965d2757
Thorsten Lorenz <thlorenz@gmx.de>
2013-10-31 10:22:22 -0400

    adding process-package tests and fixing bug in it

--- test/process-package.js
+++ test/process-package.js
@@ -23,3 +23,48 @@ test('\nprocessing the root fixture path', function (t) {
     t.end()
   })
 })
+
+test('\nprocessing foo path', function (t) {
+  processPackage(fixtures + '/node_modules/foo', function (err, res) {
+    if (err) return t.fail(err);
+    var scssFiles = res.scssFiles.map(subdir);
+    var deps = res.deps.map(subdir);
+    t.deepEqual(scssFiles, [ '/node_modules/foo/node_modules/fooz/sass/index.scss' ], 'resolves fooz scss index')
+    t.deepEqual(deps, [ '/node_modules/foo/node_modules/fooz' ], 'resolves fooz directory')
+    t.end()
+  })
+})
+
+test('\nprocessing foo/fooz path', function (t) {
+  processPackage(fixtures + '/node_modules/foo/node_modules/fooz', function (err, res) {
+    if (err) return t.fail(err);
+    var scssFiles = res.scssFiles.map(subdir);
+    var deps = res.deps.map(subdir);
+    t.deepEqual(scssFiles, [ ], 'resolves no scssFiles')
+    t.deepEqual(deps, [ ], 'resolves no deps')
+    t.end()
+  })
+})
+
+test('\nprocessing bar path', function (t) {
+  processPackage(fixtures + '/node_modules/bar', function (err, res) {
+    if (err) return t.fail(err);
+    var scssFiles = res.scssFiles.map(subdir);
+    var deps = res.deps.map(subdir);
+
+    t.deepEqual(scssFiles, [ '/node_modules/bar/node_modules/baz/sass/index.scss' ], 'resolves baz scss index')
+    t.deepEqual(deps, [ '/node_modules/bar/node_modules/baz' ], 'resolves baz directory')
+    t.end()
+  })
+})
+
+test('\nprocessing bar/baz path', function (t) {
+  processPackage(fixtures + '/node_modules/bar/node_modules/baz', function (err, res) {
+    if (err) return t.fail(err);
+    var scssFiles = res.scssFiles.map(subdir);
+    var deps = res.deps.map(subdir);
+    t.deepEqual(scssFiles, [ ], 'resolves no scssFiles')
+    t.deepEqual(deps, [ ], 'resolves no deps')
+    t.end()
+  })
+})
