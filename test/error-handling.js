2b098db0e7b040c182e1b219458952d9345449b0
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-01 11:38:18 -0400

    not running tests that require sass on travis

--- test/error-handling.js
+++ test/error-handling.js
@@ -1,14 +1,18 @@
 'use strict';
 /*jshint asi: true */
 
+
 var test = require('tap').test
 var path = require('path')
 var sassResolve = require('../')
 var fixtures = path.join(__dirname, 'fixtures-broken');
 
-test('\nwhen creating imports from fixture with broken scss', function (t) {
-  sassResolve(fixtures, path.join(fixtures, 'index.css'), function (err) {
-    t.ok(err, 'calls back with error');
-    t.end()
-  });
-})
+// This test requires sass to be installed, so lets not run it on travis
+if (!process.env.TRAVIS) {
+  test('\nwhen creating imports from fixture with broken scss', function (t) {
+    sassResolve(fixtures, path.join(fixtures, 'index.css'), function (err) {
+      t.ok(err, 'calls back with error');
+      t.end()
+    });
+  })
+}
