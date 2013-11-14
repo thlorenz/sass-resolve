4f0b809296968dce18217ebf48744eb969fb97dc
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-14 12:03:26 -0500

    reproducing punycode bug

--- test/punycode-bug-resolve.js
+++ test/punycode-bug-resolve.js
@@ -4,7 +4,7 @@
 var test = require('tap').test
 var path = require('path')
 var resolveSassPaths = require('../lib/resolve-sass-paths')
-var fixtures = path.join(__dirname, 'fixtures');
+var fixtures = path.join(__dirname, 'fixtures-punycode-bug');
 
 function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
@@ -15,18 +15,16 @@ function subdir(dir) {
 }
 
 test('\nresolving sass paths starting in the fixtures main directory', function (t) {
-  resolveSassPaths(path.join(__dirname, 'fixtures'),  function (err, res) {
+  resolveSassPaths(fixtures,  function (err, res) {
     if (err) return console.error(err);
     var dirs = res.map(subdir);
 
-    t.equal(dirs.length, 4, 'resolves 4 scss files');
-    [ '/node_modules/foo/sass/index.scss',
-      '/node_modules/foo/node_modules/fooz/sass/index.scss',
-      '/node_modules/bar/node_modules/baz/sass/index.scss',
-      '/sass/index.scss' ].forEach(function (dir) {
+    inspect(res)
+
+    t.equal(dirs.length, 1, 'resolves one scss file');
+    [ '/sass/index.scss' ].forEach(function (dir) {
         t.ok(~dirs.indexOf(dir), 'resolves ' + dir)
     });
     t.end()
   })
 })
-
